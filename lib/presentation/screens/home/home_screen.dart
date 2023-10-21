ff55f822dca2a284846ec3afaaed2cdf041e5c3f
Emilio Gajardo <ej.gajardo@gmail.com>
2023-10-20 22:06:15 -0300

    Implementación de: HomeScreen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,44 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Flutter + Material 3'),
         centerTitle: false,
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItems = appMenuItems[index];
+        return _CustonListTile(menuItems: menuItems);
+      }
+    );
+  }
+}
+
+class _CustonListTile extends StatelessWidget {
+  const _CustonListTile({required this.menuItems});
+
+  final MenuItem menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItems.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(menuItems.title),
+      subtitle: Text(menuItems.subTitle),
+      onTap: () {
+        /// todo: navegar a otras pantallas
+        /// Navigator.of(context).pushNamed(menuItems.link);
+      },
     );
   }
 }
