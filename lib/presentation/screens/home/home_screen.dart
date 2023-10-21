4d8a2c46f8d127c95a98f3fb6590e4b66812a7bd
Emilio Gajardo <ej.gajardo@gmail.com>
2023-10-21 16:34:37 -0300

    Configuración de rutas con nombre

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -54,6 +55,7 @@ class _CustonListTile extends StatelessWidget {
         //   ),
         // );
         // Navigator.pushNamed(context, menuItem.link);
+        // context.pushNamed(CardsScreen.name);
         context.push(menuItem.link);
       },
     );
