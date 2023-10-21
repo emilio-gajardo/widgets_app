6982579caa6ac8d5fa025d9b95a0cae237d92d2d
Emilio Gajardo <ej.gajardo@gmail.com>
2023-10-20 21:02:28 -0300

    modificación: de estilos de AppBar

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -30,5 +30,8 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
     colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
   );
 }
